52aa7e5d1e1b42fe678c95b6a11e521a6b08b929
ZigZagT <i@zhangtao.cool>
2021-07-03 19:27:13 -0700

    ubuntu-base: allow set TZ/APT with non-root user

--- ubuntu-base/entrypoint.sh
+++ ubuntu-base/entrypoint.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env bash
 
-# set correct time zone
-echo $TZ > /etc/timezone
-rm -f /etc/localtime
-ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
-dpkg-reconfigure -f noninteractive tzdata 2>/dev/null
-
-# set apt mirror
-if [[ -n $APT_MIRROR ]]; then
-    sed -i "s#$(cat apt-mirror.txt)#$APT_MIRROR#g" /etc/apt/sources.list
-fi
-
+sudo /container-setup.sh
 exec "$@"
