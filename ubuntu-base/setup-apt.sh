0eb11bf075a3684eb5578b6b306816329687151f
ZigZagT <i@zhangtao.cool>
2022-08-05 20:12:22 -0700

    update setup-apt

--- ubuntu-base/setup-apt.sh
+++ ubuntu-base/setup-apt.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 set -e
 
+VERSION_CODENAME=$(grep VERSION_CODENAME /etc/os-release | cut -d '=' -f 2)
+SOURCE_LIST=$(cat <<-EOL
+deb ${APT_MIRROR} ${VERSION_CODENAME} main restricted
+deb ${APT_MIRROR} ${VERSION_CODENAME}-updates main restricted
+deb ${APT_MIRROR} ${VERSION_CODENAME} universe
+deb ${APT_MIRROR} ${VERSION_CODENAME}-updates universe
+deb ${APT_MIRROR} ${VERSION_CODENAME} multiverse
+deb ${APT_MIRROR} ${VERSION_CODENAME}-updates multiverse
+deb ${APT_MIRROR} ${VERSION_CODENAME}-backports main restricted universe multiverse
+deb ${APT_MIRROR} ${VERSION_CODENAME}-security main restricted
+deb ${APT_MIRROR} ${VERSION_CODENAME}-security universe
+deb ${APT_MIRROR} ${VERSION_CODENAME}-security multiverse
+EOL
+)
+
 # set apt mirror
 if [[ -n "$APT_MIRROR" ]]; then
-    sed -i "s#$(cat apt-mirror.txt)#$APT_MIRROR#g" /etc/apt/sources.list
-    echo "$APT_MIRROR" > /apt-mirror.txt
+    echo "$SOURCE_LIST" > /etc/apt/sources.list
 fi
